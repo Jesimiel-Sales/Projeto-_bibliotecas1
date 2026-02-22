6ef924d4039946c6e046c587f85d3d916b507c4c
Jesimiel <gsallesmoreira2@gmail.com>
2026-02-22 17:15:38 -0300

    :wq teste de novo
    :wq
    x
    ^X

---
+++ biblioteca.py
@@ -0,0 +1,27 @@
+class Biblioteca:
+    bibliotecas = []
+    def __init__(self, nome):
+        self.nome = nome
+        self._ativo = False
+        Biblioteca.bibliotecas.append(self)
+
+    def __str__(self):
+        return self.nome
+    
+    def listar_bibliotecas():
+        for biblioteca in Biblioteca.bibliotecas:
+            print(f"{biblioteca.nome} | {biblioteca.ativo}")
+    
+    def alterna_estado(self):
+        self._ativo = not self._ativo
+
+    @property
+    def ativo(self):
+        return "ativada" if self._ativo else "desativada"
+
+biblioteca_cidade = Biblioteca("Biblioteca da Cidade")
+biblioteca_shopping = Biblioteca("Biblioteca do Shopping")
+
+biblioteca_cidade.alterna_estado()
+
+Biblioteca.listar_bibliotecas()
